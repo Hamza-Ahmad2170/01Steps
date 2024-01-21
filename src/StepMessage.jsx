6022757168fb830cc515a0954e22057789d2a364
hasaigal <hasaigal@gmail.com>
2024-01-21 05:15:22 +0500

    react

---
+++ src/StepMessage.jsx
@@ -0,0 +1,10 @@
+function StepMessage({ children, step, }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3> 
+      {children}
+    </div>
+  );
+}
+
+export default StepMessage;
